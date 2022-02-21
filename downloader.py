7f0d590dd8dba358eb1dd8ccf8c5ed5302b24cd5
Rodrigo Oliveira <rodrikpoliveira@gmail.com>
2022-02-20 22:16:25 -0300

    Fix to follow Windows naming convetions

--- downloader.py
+++ downloader.py
@@ -8,7 +8,7 @@ dl_path = user_profile + '/downloads'
 def mp4(links, path=dl_path):
     for video in links:
         yt = YouTube(video)
-        title = yt.title
+        title = clean_title(yt.title)
         print(f'Downloading {title}')
         yt.streams.get_highest_resolution().download(filename=f'{title}.mp4', output_path=path)
         print(f'{title} has been downloaded.')
@@ -17,7 +17,16 @@ def mp4(links, path=dl_path):
 def mp3(links, path=dl_path):
     for video in links:
         yt = YouTube(video)
-        title = yt.title
+        title = clean_title(yt.title)
         print(f'Downloading {title}')
         yt.streams.get_audio_only().download(filename=f'{title}.mp3', output_path=path)
         print(f'{title} has been downloaded')
+
+
+def clean_title(title: str):
+    REMOVE = '|"?\/<>:*'
+
+    for character in REMOVE:
+        title = title.replace(character, '')
+
+    return title
